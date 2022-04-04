d70958c1ac1d2ba2f3551fa7c27ec7a580e07ee4
Oleh Tolmachov <kontorskiy777@gmail.com>
2022-04-04 13:31:21 +0300

    GCP provider

---
+++ provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    google = {
+      version = "4.15.0"
+      source  = "hashicorp/google"
+    }
+
+  }
+}
+provider "google" {
+  project     = var.project
+  region      = var.region
+  zone        = var.zone
+  credentials = var.credentials
+}
+
+
